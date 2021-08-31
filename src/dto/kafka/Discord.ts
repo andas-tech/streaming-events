3910ef10c6708ee6869cf9edceb59930db1d68bc
nic seltzer <nic@nicseltzer.com>
2021-08-30 21:55:31 -0500

    Adding in discord enum.

---
+++ src/dto/kafka/Discord.ts
@@ -0,0 +1,6 @@
+export enum DiscordKafkaTopics {
+    KAFKA_TOPIC_DISCORD_MESSAGE_FALLTHROUGH = "discord_message_fallthrough",
+    KAFKA_TOPIC_DISCORD_MESSAGE_CREATE = "discord_message_create",
+    KAFKA_TOPIC_DISCORD_MESSAGE_UPDATE = "discord_message_update",
+    KAFKA_TOPIC_DISCORD_PRESENCE_UPDATE = "discord_presence_update",
+}
