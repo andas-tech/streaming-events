63e1ba938ce3de218b168bbc9e8459c3d4c14a51
nic seltzer <nic@nicseltzer.com>
2021-10-09 10:25:55 -0500

    Adding twitch types.

---
+++ dist/Twitch.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TwitchKafkaTopics = void 0;
+var TwitchKafkaTopics;
+(function (TwitchKafkaTopics) {
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_BITS_CREATE"] = "twitch_bits_create";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_FOLLOW_CREATE"] = "twitch_follow_create";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_FOLLOW_UPDATE"] = "twitch_follow_update";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_FOLLOW_DELETE"] = "twitch_follow_delete";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_MESSAGE_CREATE"] = "twitch_message_create";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_MESSAGE_DELETE"] = "twitch_message_delete";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_POINTS_CREATE"] = "twitch_points_create";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_RAID_CREATE"] = "twitch_raid_create";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_SUB_CREATE"] = "twitch_sub_create";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_SUB_UPDATE"] = "twitch_sub_update";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_SUB_DELETE"] = "twitch_sub_delete";
+    TwitchKafkaTopics["KAFKA_TOPIC_TWITCH_SUB_GIFT"] = "twitch_sub_gift";
+})(TwitchKafkaTopics = exports.TwitchKafkaTopics || (exports.TwitchKafkaTopics = {}));
---
+++ src/types/kafka/Twitch.ts
@@ -0,0 +1,14 @@
+export enum TwitchKafkaTopics {
+    KAFKA_TOPIC_TWITCH_BITS_CREATE = "twitch_bits_create",
+    KAFKA_TOPIC_TWITCH_FOLLOW_CREATE = "twitch_follow_create",
+    KAFKA_TOPIC_TWITCH_FOLLOW_UPDATE = "twitch_follow_update",
+    KAFKA_TOPIC_TWITCH_FOLLOW_DELETE = "twitch_follow_delete",
+    KAFKA_TOPIC_TWITCH_MESSAGE_CREATE = "twitch_message_create",
+    KAFKA_TOPIC_TWITCH_MESSAGE_DELETE = "twitch_message_delete",
+    KAFKA_TOPIC_TWITCH_POINTS_CREATE = "twitch_points_create",
+    KAFKA_TOPIC_TWITCH_RAID_CREATE = "twitch_raid_create",
+    KAFKA_TOPIC_TWITCH_SUB_CREATE = "twitch_sub_create",
+    KAFKA_TOPIC_TWITCH_SUB_UPDATE = "twitch_sub_update",
+    KAFKA_TOPIC_TWITCH_SUB_DELETE = "twitch_sub_delete",
+    KAFKA_TOPIC_TWITCH_SUB_GIFT = "twitch_sub_gift",
+}
